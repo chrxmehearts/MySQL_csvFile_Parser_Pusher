ea32b1a194a4c248f9619a2a56643832e9dfefb2
chrxmehearts <sengvihss@gmail.com>
2025-02-19 18:05:26 +0100

    update program class

--- csvParsNPush/Program.cs
+++ csvParsNPush/Program.cs
@@ -4,6 +4,11 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        string csvPath = "your csv path";
+        string connectionString = "your mysql connection";
+        int countOfFields = 5; //type count of fields in your csv file
+        Parser parser = new Parser();
+        Pusher pusher = new Pusher();
         
     }
 }
