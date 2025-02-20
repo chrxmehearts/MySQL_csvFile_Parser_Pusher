a78cc578bcbde32246dc9cc1742d4e3b3821b709
chrxmehearts <sengvihss@gmail.com>
2025-02-20 14:49:40 +0100

    add method which asking csv file from user

--- csvParsNPush/Program.cs
+++ csvParsNPush/Program.cs
@@ -4,19 +4,53 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        string csvPath = "your csv file path";
+        string csvPath = GetCsvFilePath();
+        
         string connectionString = "Server=yourServer;Database=yourDB;User Id=your user id ;Password=your password;";
         string tableName = "your table name ";
         int countOfFields = 5; //insert count of fields in your csv file
-        
+
         Parser parser = new Parser();
         Pusher pusher = new Pusher(connectionString, tableName);
 
         List<Dictionary<string, string?>> data = parser.ParseCsv(csvPath, countOfFields);
 
         logOfParsedData(data);
-        
+
         pusher.PushToDB(data);
+    }
+
+    private static string GetCsvFilePath()
+    {
+        string? filePath;
+        while (true)
+        {
+            Console.WriteLine("Please enter your csv file path:");
+            filePath = Console.ReadLine();
+            
+            if (string.IsNullOrEmpty(filePath))
+            {
+                Console.WriteLine("Error: File path cannot be empty. Try again.");
+                continue;
+            }
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Error: File not found. Please enter a valid file path.");
+                continue;
+            }
+
+            if (Path.GetExtension(filePath) != ".csv")
+            {
+                Console.WriteLine("Error: The file must be a CSV. Try again.");
+                continue;
+            }
+
+            break;
+        }
+
+
+        return filePath;
     }
 
     private static void logOfParsedData(List<Dictionary<string, string?>> data)
